e4772c82833923d5a21565ef9196fd8779dafd38
Jeevanandham S <sj12042007@gmail.com>
2026-02-22 17:02:34 +0530

    medium logic

---
+++ 2D Array/PalindromicRowsCount.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, count = 0;
+    scanf("%d %d", &n, &m);
+
+    int arr[n][m];
+    for (int i=0;i<n;i++) {
+        for (int j=0; j<m;j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (int i=0;i<n;i++) {
+        int isPalindrome = 1;
+        for (int j=0;j<m/2;j++) {
+            if (arr[i][j]!=arr[i][m-1-j]) {
+                isPalindrome=0;
+                break;
+            }
+        }
+        if (isPalindrome) {
+            count++;
+        }
+    }
+
+    printf("%d\n", count);
+    return 0;
+}
