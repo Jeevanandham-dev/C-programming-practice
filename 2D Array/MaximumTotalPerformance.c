142bcb4c827ec259d8a950dd2df73b85be072828
Jeevanandham S <sj12042007@gmail.com>
2026-02-24 21:05:29 +0530

    medium logic

---
+++ 2D Array/MaximumTotalPerformance.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+int main() {
+    int n,m;
+    scanf("%d %d", &n,&m);
+    int arr[n][m];
+    for (int i=0;i<n;i++) {
+        for (int j=0;j<m; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    int maxSum = INT_MIN;  
+    int maxCol = 0;
+    for (int j=0;j<m;j++) {
+        int colSum = 0;
+        for (int i=0; i<n; i++) {
+            colSum += arr[i][j];
+        }
+        if (colSum>maxSum) {
+            maxSum = colSum;
+            maxCol = j;
+        }
+    }
+    printf("%d\n", maxCol);
+    return 0;
+}
