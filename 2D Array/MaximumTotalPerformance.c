85423d3762ab2308a3439fa4846acad84f85616a
Jeevanandham S <sj12042007@gmail.com>
2026-02-24 21:35:31 +0530

    medium logic

--- 2D Array/MaximumTotalPerformance.c
+++ 2D Array/MaximumTotalPerformance.c
@@ -2,7 +2,7 @@
 #include <limits.h>
 int main() {
     int n,m;
-    scanf("%d %d", &n,&m);
+    scanf("%d\n%d", &n,&m);
     int arr[n][m];
     for (int i=0;i<n;i++) {
         for (int j=0;j<m; j++) {
