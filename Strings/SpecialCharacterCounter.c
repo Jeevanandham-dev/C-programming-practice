38bcf460def94ef0a16b1df65bf2c911181dfbf8
Jeevanandham S <sj12042007@gmail.com>
2026-02-27 15:48:43 +0530

    easy

---
+++ Strings/SpecialCharacterCounter.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int count = 0;
+    char arr[30];
+    scanf("%s", arr);
+    for(int i =0;arr[i]!='\0';i++){
+        if(arr[i]=='@'||arr[i]=='#'||arr[i]=='$'||arr[i]=='%'||arr[i]=='&'){
+            count++;
+        }
+    }
+
+    printf("%d", count);
+}
